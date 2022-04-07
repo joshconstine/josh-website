95ce9d343deecd565e8c38faae2cfafb41339f57
joshua constine <joshua.constine97@gmail.com>
2022-04-07 12:18:01 -0500

    new changes

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
+
+const Navbar = () => {
+  //we take in logout and current user so our navbar can display accuratley
+
+  return (
+    // <>
+    <AppBar
+      position="static"
+      sx={{
+        borderColor: "black",
+      }}
+    >
+      <Container
+        sx={{
+          mr: 2,
+          display: "flex",
+          flexDirection: { xs: "column", sm: "column", md: "row" },
+          justifyContent: "space-between",
+          alignItems: { xs: "center", sm: "center", md: "flex-end" },
+        }}
+      >
+        <h1>hellow orld</h1>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
