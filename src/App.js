771c48e569648cb4b79baa4ce299f480c04bd6b7
joshua constine <joshua.constine97@gmail.com>
2022-04-07 13:02:58 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Joshua Constine</h1>
-      </header>
+      <Box className="Appbody">
+        <h2>Joshua Constine</h2>
+        <h4>Software Engineer</h4>
+        <img src="faceShot.JPG" alt="img" className="img" />
+        <p>information about me here !!!!!!!!</p>
+      </Box>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
           alignItems: { xs: "center", sm: "center", md: "flex-end" },
         }}
       >
-        <h1>hellow orld</h1>
+        <h1>Joshua Constine</h1>
       </Container>
     </AppBar>
   );
